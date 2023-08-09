252fcaaed66519671ebb46ae0661adb83b85ed3b
emily27yee <139501637+emily27yee@users.noreply.github.com>
2023-08-09 15:20:30 -0400

    Added checkWin and fixed collectedFood array

--- burger.js
+++ burger.js
@@ -115,7 +115,7 @@ setSolids([player])
 let level = 0
 const levels = [
   map`
-.....
+1....
 .....
 .....
 .....
@@ -128,12 +128,15 @@ setPushables({
   [ player ]: []
 })
 
-let collectedFood = [];
+const level1burger = ['m', 'c', 'b'];
+const burgerLevels = [level1burger];
+let collectedFood1 = [];
+let collectedFood2 = [];
 
 onInput("a", () => {
   getFirst(player).x -= 1
   
-  collectedFood.forEach((food) => {
+  collectedFood1.forEach((food) => {
     food.x = getFirst(player).x
   })
 })
@@ -141,7 +144,7 @@ onInput("a", () => {
 onInput("d", () => {
   getFirst(player).x += 1
   
-  collectedFood.forEach((food) => {
+  collectedFood1.forEach((food) => {
     food.x = getFirst(player).x
   })
 })
@@ -157,7 +160,11 @@ let foods = [];
 
 function addFood() {
   let x = Math.floor(Math.random() * 5);
-  let y = 0;
+  let y
+  if (x == 0)
+    y = 1;
+  else
+    y = 0;
   let newFood = addSprite(x, y, randFood());
   foods.push(newFood);
 }
@@ -165,7 +172,9 @@ function addFood() {
 function fallingFood() {
   foodTypes.forEach(type => {
     getAll(type).forEach(food => {
-      food.y += 1
+      setTimeout(() => {
+        food.y += 1;
+      }, 1000)
     }) 
   })
 }
@@ -173,11 +182,10 @@ function fallingFood() {
 function removeFood() {
   foodTypes.forEach(type => {
     getAll(type).forEach(food => {
-      if (food.y == 4) {
+      if (food.y == 4 && !(food.x === getFirst(player).x)) {
         let x = food.x;
         let y = food.y;
-        food.remove();
-        clearTile(x, y);
+      food.remove();
       }
     })
   })
@@ -187,14 +195,54 @@ function stackingFood() {
   foodTypes.forEach(type => {
     getAll(type).forEach(food => {
       if (food.x === getFirst(player).x && food.y === getFirst(player).y) {
-        collectedFood.push(food)
+        collectedFood1.push(food)
+        collectedFood2.push(food.type)
+        console.log(collectedFood2)
       }
     })
   })
+}
+
+let win = false;
+function checkWin() {
+  if (burgerLevels[level].length !== collectedFood1.length) {
+    win = false;
+  }
+  // not working yet
+  for (let i = 0; i < burgerLevels[level].length && collectedFood2.length; i++) {
+    if (burgerLevels[level][i] === collectedFood2[i]) {
+      win = true;
+    }
+    else {
+      win = false;
+      break;
+    }
+  }
+  if (win) {
+    addText("You Win", {
+              x: 10,
+              y: 0,
+              color: color`black`
+            });
+  }
+  else {
+      addText("You Lose", {
+              x: 10,
+              y: 0,
+              color: color`black`
+            });
+  }
+  clearInterval(runGame);
 }
 
 var runGame = setInterval(() => {
   addFood();
   fallingFood();
   removeFood();
+  stackingFood();
+  // this works
+  if (collectedFood2.includes(topBun)) {
+    console.log("Finished")
+    checkWin();
+  }
 }, 1000);
